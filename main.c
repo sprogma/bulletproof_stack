61b377378fb933767e104cb16a77d138db5ecce7
sprogma <mux-228@yandex.ru>
2025-09-29 08:15:08 +0300

    add calculator

--- main.c
+++ main.c
@@ -1,417 +1,98 @@
 #include "stack.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "math.h"
 
 
-#define START_TESTING_FUNCTION printf("------------------ testing function %s ------------------\n", __FUNCTION__)
-#define END_TESTING_FUNCTION do{printf("WRONG BEHAVIOUR: TESTING FUNCTION %s WAS NOT FAILED\n", __FUNCTION__); abort(); } while (0)
-
-#define $(f) do{enum stack_error_code err = 0; if ((err = f) != 0) { printf("OK: CodeLine %s:%s:%d [%s] returned error %d (%s)\n", __FILE__, __FUNCTION__, __LINE__, #f, err, stack_get_error_string(err)); return; }}while(0)
-
-void test_000()
-{
-    START_TESTING_FUNCTION;
-    /* this must show warning: -Wunused-result :) */
-    if (0)
-    {
-        struct stack_t stk1;
-        stack_init(&stk1);
-    }
-    printf("Do you see warning?\n");
-}
-
-void test_050()
-{
-    START_TESTING_FUNCTION;
-    $(stack_init(NULL));
-    END_TESTING_FUNCTION;
-}
-
-void test_051()
-{
-    START_TESTING_FUNCTION;
-    $(stack_push(NULL, 0));
-    END_TESTING_FUNCTION;
-}
-
-void test_052()
-{
-    START_TESTING_FUNCTION;
-    $(stack_pop(NULL, NULL));
-    END_TESTING_FUNCTION;
-}
-
-void test_053()
-{
-    START_TESTING_FUNCTION;
-    $(stack_destroy(NULL));
-    END_TESTING_FUNCTION;
-}
-
-void test_054()
-{
-    START_TESTING_FUNCTION;
-    $(stack_get_size(NULL, NULL));
-    END_TESTING_FUNCTION;
-}
-
-void test_100()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1;
-
-    $(stack_init(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_112()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1;
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_125()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_137()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1;
-
-    $(stack_push(&stk1, 0));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_138()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_push(&stk1, 0));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_143()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1;
-
-    $(stack_pop(&stk1, NULL));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_144()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_pop(&stk1, NULL));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_150()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    $(stack_destroy(&stk1));
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_175()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    $(stack_init(&stk1));
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_200()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    $(stack_pop(&stk1, NULL));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_300()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    for (int i = 0; i < 128; ++i)
-    {
-        $(stack_push(&stk1, i));
-    }
-    for (int i = 128 - 1; i >= 0; --i)
-    {
-        stack_value_t out = 0;
-        $(stack_pop(&stk1, &out));
-        if (out != i)
-        {
-            printf("ERROR: Push and pop order is wrong; get %d instead of %d\n", out, i);
-        }
-    }
-    printf("OK: Test passed\n");
-}
-
-void test_325()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    $(stack_push(&stk1, 1));
-    $(stack_push(&stk1, 2));
-
-    stk1.data[0] += 1;
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_337()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    stk1.data_len = 1;
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_350()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    stack_value_t x = 1;
-
-    $(stack_push(&stk1, x));
-
-    $(stack_push(&stk1, x + 1));
-
-    $(stack_push(&stk1, x + 2));
-
-    $(stack_push(&stk1, x + 3));
-
-    stk1.data[0] = 0;
-
-    $(stack_pop(&stk1, &x));
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_375()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    stack_value_t x = 1;
-
-    for (int i = 0; i < 1024; ++i)
-    {
-        $(stack_push(&stk1, x + i));
-    }
-
-    stk1.data[0] = 0;
-
-    $(stack_pop(&stk1, &x));
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_400()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    stk1.data[0] = 0;
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_500()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    stk1.data = calloc(16, sizeof(*stk1.data));
-    stk1.data_len = 1;
-    stk1.data_alloc = 16;
-    stk1.data[0] = 0;
-
-    $(stack_destroy(&stk1));
-
-    END_TESTING_FUNCTION;
-}
-
-void test_600()
-{
-    START_TESTING_FUNCTION;
-
-    struct stack_t stk1 = {};
-
-    $(stack_init(&stk1));
-
-    /* to break another stacks, increase number of threads and number of iterations */
-    {
-        int skip = 0;
-        #pragma omp parallel for num_threads(2) private(skip)
-        for (int i = 0; i < 4096; ++i)
-        {
-            // printf("%d\n", skip);
-            if (skip == 1 || stack_push(&stk1, i) != 0)
-            {
-                skip = 1;
-            }
-        }
-    }
-
-    for (int i = 4096 - 1; i >= 0; --i)
-    {
-        stack_value_t out;
-        $(stack_pop(&stk1, &out));
-        if (out != i)
-        {
-            printf("This stack doesn't support multithreading, Does it say this? (do you see upper error?)\n");
-            printf("at pop of %d found %d\n", i, out);
-            return;
-        }
-    }
-
-    if (stk1.data_len == 0)
-    {
-        $(stack_destroy(&stk1));
-        
-        printf("This stack is lucky, or supports multithreading.\n");
-        return;
-    }
-    
-    $(stack_destroy(&stk1));
-    
-    END_TESTING_FUNCTION;
-}
-
-const struct stack_t test_700_glob_stk = {};
-
-void test_700()
-{
-    START_TESTING_FUNCTION;
-    
-    $(stack_init((void *)&test_700_glob_stk));
-    
-    END_TESTING_FUNCTION;
-}
-
-void test_800()
-{
-    START_TESTING_FUNCTION;
-
-    $(stack_init((void *)0x0000000011223344LL));
-    
-    END_TESTING_FUNCTION;
-}
-
-
-
+#define $(call) do {int err; if ((err = (call))) { fprintf(stderr, "stack operation returned error: %d (%s)\n", err, stack_get_error_string(err)); return 1; }} while (0)
 
 
 int main()
 {
-    test_000();
-    test_050();
-    test_051();
-    test_052();
-    test_053();
-    test_054();
-    test_100();
-    test_112();
-    test_125();
-    test_137();
-    test_138();
-    test_143();
-    test_144();
-    test_150();
-    test_175();
-    test_200();
-    test_300();
-    test_325();
-    test_337();
-    test_350();
-    test_375();
-    test_400();
-    test_500();
-    test_600();
-    test_700();
-    test_800();
+    struct stack_t stk = {};
+
+    $(stack_init(&stk));
+
+    char command[64] = "";
+
+    while (1)
+    {
+        if (scanf("%s63", command) != 1)
+        {
+            fprintf(stderr, "program must be stopped by hlt command.\n");
+        }
+        if (strcmp(command, "push") == 0)
+        {
+            int x = 0;
+            if (scanf("%d", &x) != 1)
+            {
+                fprintf(stderr, "wrong push format. number expected");
+            }
+            $(stack_push(&stk, x));
+        }
+        else if (strcmp(command, "pop") == 0)
+        {
+            int x = 0;
+            $(stack_pop(&stk, &x));
+            printf("%d\n", x);
+        }
+        else if (strcmp(command, "dup") == 0)
+        {
+            int x = 0;
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, x));
+            $(stack_push(&stk, x));
+        }
+        else if (strcmp(command, "add") == 0)
+        {
+            int x = 0, y = 0;
+            $(stack_pop(&stk, &y));
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, x + y));
+        }
+        else if (strcmp(command, "mul") == 0)
+        {
+            int x = 0, y = 0;
+            $(stack_pop(&stk, &y));
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, x * y));
+        }
+        else if (strcmp(command, "sub") == 0)
+        {
+            int x = 0, y = 0;
+            $(stack_pop(&stk, &y));
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, x - y));
+        }
+        else if (strcmp(command, "div") == 0)
+        {
+            int x = 0, y = 0;
+            $(stack_pop(&stk, &y));
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, x / y));
+        }
+        else if (strcmp(command, "sqrt") == 0)
+        {
+            int x = 0;
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, sqrt(x)));
+        }
+        else if (strcmp(command, "sqrt") == 0)
+        {
+            int x = 0;
+            $(stack_pop(&stk, &x));
+            $(stack_push(&stk, sqrt(x)));
+        }
+        else if (strcmp(command, "hlt") == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            fprintf(stderr, "unknown command: %s\n", command);
+        }
+        command[0] = '\0';
+    }
+    
     return 0;
 }
